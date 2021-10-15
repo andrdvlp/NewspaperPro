ec887ee566e09ab559bc653a90e2f21f73e5adca
Andrey <ankrnv@gmail.com>
2021-10-15 16:39:23 +0300

    Andrey Korneev Add Room

--- app/src/main/java/com/example/newspaper/view/fragments/DetailsFragment.kt
+++ app/src/main/java/com/example/newspaper/view/fragments/DetailsFragment.kt
@@ -1,15 +1,21 @@
 package com.example.newspaper.view.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.example.newspaper.data.ApiConstants
+import com.example.newspaper.data.Entity.Article
 import com.example.newspaper.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailsBinding
+    private lateinit var article: Article
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +24,29 @@ class DetailsFragment : Fragment() {
     ): View? {
         binding = FragmentDetailsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setNewsDetails()
+
+        binding.detailsFabMarked.setOnClickListener {
+
+        }
+
+        binding.detailsFab.setOnClickListener {
+        }
+
+    }
+
+    fun setNewsDetails() {
+        article = arguments?.get("artitcle") as Article
+
+        binding.detailsToolbar.title = article.title
+        Glide.with(this)
+            .load(article.urlToImage)
+            .centerCrop()
+            .into(binding.app_bar_image)
     }
 }
